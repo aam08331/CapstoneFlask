4ecfd3853d37a23612cab046857c66c68f83563e
aam08331 <112673088+aam08331@users.noreply.github.com>
2025-04-06 04:13:27 -0400

    Update pages.py

--- Flask_Test/board/pages.py
+++ Flask_Test/board/pages.py
@@ -82,7 +82,7 @@ def run_scrape_job(job_id):
 
         # Path to your ChromeDriver
         from selenium.webdriver.chrome.service import Service
-        driver = webdriver.Remote(command_executor="https://standalone-chrome-production-fc7a.up.railway.app/:4444", options=chrome_options)
+        driver = webdriver.Remote(command_executor="https://standalone-chrome-production-fc7a.up.railway.app/", options=chrome_options)
 
         # Open the login page
         driver.get("https://iot.achtbytes.com/copc/tenant")
